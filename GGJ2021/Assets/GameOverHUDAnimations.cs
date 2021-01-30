4c95fb21232b5fbb9c7b1ce68a4c7c01a307c34e
JoaoPardal <jflpardal@gmail.com>
2021-01-30 21:06:45 +0000

    Animations for game over states: 'mustache found owner' and 'mustache arrested'

---
+++ GGJ2021/Assets/GameOverHUDAnimations.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverHUDAnimations : MonoBehaviour
+{
+    [SerializeField] private bool m_FoundOwner = false;
+    [SerializeField] private bool m_Arrested = false;
+
+    const string animator_trigger_foundOwner = "FoundOwner";
+    const string animator_trigger_arrested = "Arrested";
+
+    private Animator m_Animator = null;
+
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if(m_FoundOwner)
+        {
+            m_Animator.SetTrigger(animator_trigger_foundOwner);
+            m_FoundOwner = false;
+        }
+        else if(m_Arrested)
+        {
+            m_Animator.SetTrigger(animator_trigger_arrested);
+            m_Arrested = false;
+        }
+    }
+}
--- GGJ2021/Assets/GameState.cs
+++ GGJ2021/Assets/GameState.cs
@@ -1,18 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameState : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private bool m_IsGameOver = false;
+    [SerializeField] private Canvas m_FadeCanvas = null;
+    [SerializeField] private GameObject m_HUDToShowAfterFade = null;
+    [SerializeField] private Button m_RestartButton = null;
 
-    // Update is called once per frame
+    [SerializeField][Range(1.5f, 4)] private float m_ScalingFactor = 2f;
+    [SerializeField][Range(1.5f, 4)] private float m_ScalingDuration = 1.5f;
+    const string tag_HudToShowAfterFade = "HUD";
+    const string tag_FadeCanvas = "Fade";
+
     void Update()
     {
-        
+        if(m_IsGameOver)
+        {
+            FadeToBlack();
+            m_IsGameOver = false;
+        }
+    }
+
+    private void FadeToBlack()
+    {
+        foreach(var image in m_FadeCanvas.GetComponentsInChildren<Image>())
+        {
+            if(image.gameObject.CompareTag(tag_FadeCanvas))
+            {
+                image.color = Color.black;
+                if(m_HUDToShowAfterFade.CompareTag(tag_HudToShowAfterFade))
+                {
+                    Vector3 scalingFactor = new Vector3(m_ScalingFactor, m_ScalingFactor, 1);
+                    LeanTween.scale(m_HUDToShowAfterFade, scalingFactor, m_ScalingDuration);
+                    m_RestartButton.gameObject.SetActive(true);
+                }
+                else
+                {
+                    Debug.LogError("No valid HUD to resize");
+                }
+            }
+        }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
