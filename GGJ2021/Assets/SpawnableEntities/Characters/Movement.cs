bc00d311017b00bab73039d45f253717e86328b6
JoaoPardal <jflpardal@gmail.com>
2021-01-30 10:39:09 +0000

    Mustache movement and animations * house cleanup

---
+++ GGJ2021/Assets/SpawnableEntities/Characters/Movement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] [Range(2, 10)] float MoveSpeed = 3;
+
+    private string animator_bool_isWalking = "isWalking";
+    private Rigidbody2D m_rb = null;
+    private SpriteRenderer m_SpriteRenderer = null;
+    private Animator m_Animator = null;
+
+    private Vector2 velocity = Vector2.zero;
+
+    void Start()
+    {
+        m_rb = GetComponent<Rigidbody2D>();
+        m_SpriteRenderer = GetComponent<SpriteRenderer>();
+        m_Animator = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        m_rb.velocity = velocity;
+
+        UpdateAnimator();
+        UpdateFacingDirection();
+    }
+
+    private void UpdateAnimator()
+    {
+        bool isWalking = (velocity.x != 0 || velocity.y != 0) ? true : false;
+        m_Animator.SetBool(animator_bool_isWalking, isWalking);
+    }
+
+    private void UpdateFacingDirection()
+    {
+        if (velocity.x != 0)
+        {
+            if (velocity.x > 0)
+            {
+                m_SpriteRenderer.flipX = true;
+            }
+            else if (velocity.x < 0)
+            {
+                m_SpriteRenderer.flipX = false;
+            }
+        }
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        velocity = context.ReadValue<Vector2>() * MoveSpeed;
+    }
+}
