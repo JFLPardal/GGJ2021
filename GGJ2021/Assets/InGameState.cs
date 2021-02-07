f1d7cc285e4955893f80003a3d6324fa894e930f
iris-rod <igprodrigues125@gmail.com>
2021-02-07 12:59:08 +0000

    Use different script to control switch scenes (maintain ingame loop as it is)

---
+++ GGJ2021/Assets/InGameState.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace GameControl
+{
+    public class InGameState : MonoBehaviour
+    {
+        [SerializeField] private bool m_IsGameOver = false;
+        [SerializeField] private Canvas m_FadeCanvas = null;
+        [SerializeField] private GameObject m_HUDToShowAfterFade = null;
+        [SerializeField] private GameOverHUDAnimations m_GameOverHUDAnimations = null;
+        [SerializeField] private Button m_RestartButton = null;
+
+        [SerializeField] [Range(1.5f, 4)] private float m_ScalingFactor = 2f;
+        [SerializeField] [Range(1.5f, 4)] private float m_ScalingDuration = 1.5f;
+
+        private bool m_FirstGameOver = true;
+
+        const string tag_HudToShowAfterFade = "HUD";
+        const string tag_FadeCanvas = "Fade";
+
+        private AudioSource source;
+
+        private void Start()
+        {
+            source = GetComponent<AudioSource>();
+        }
+
+        void Update()
+        {
+            m_IsGameOver = m_GameOverHUDAnimations.IsGameOver();
+            if (m_IsGameOver && m_FirstGameOver)
+            {
+                FadeToBlack();
+                m_FirstGameOver = false;
+            }
+        }
+
+        private void FadeToBlack()
+        {
+            foreach (var image in m_FadeCanvas.GetComponentsInChildren<Image>())
+            {
+                source.Stop();
+                if (image.gameObject.CompareTag(tag_FadeCanvas))
+                {
+                    image.color = Color.black;
+                    if (m_HUDToShowAfterFade.CompareTag(tag_HudToShowAfterFade))
+                    {
+                        Vector3 scalingFactor = new Vector3(m_ScalingFactor, m_ScalingFactor, 1);
+                        LeanTween.scale(m_HUDToShowAfterFade, scalingFactor, m_ScalingDuration);
+                        m_RestartButton.gameObject.SetActive(true);
+                    }
+                    else
+                    {
+                        Debug.LogError("No valid HUD to resize");
+                    }
+                }
+            }
+        }
+
+        public void RestartGame()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
--- GGJ2021/Assets/SpawnableEntities/GenerateOwner.cs
+++ GGJ2021/Assets/SpawnableEntities/GenerateOwner.cs
@@ -1,12 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Utils;
 
 public class GenerateOwner : MonoBehaviour
 {
     [SerializeField]
-    private Mustachometer mustacheometer;
+    private GameObject mustacheometer;
 
     [SerializeField]
     private GameObject owner;
@@ -16,34 +15,11 @@ public class GenerateOwner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        /*houses = (BuildingDoor[]) FindObjectsOfType(typeof(BuildingDoor));
-        ChoseRandomOwnerHouse();
-        SetHouseOnMustacheometer();
-        owner.transform.position = new Vector2(-15, 20);
-        owner.SetActive(false);*/
-    }
-
-    public void OnGameOver()
-    {
-        owner.SetActive(true);
-    }
-
-    public void OnStartGame()
-    {
-        Debug.Log("generate owner on start");
-        SetNecessaryObjects();
         houses = (BuildingDoor[])FindObjectsOfType(typeof(BuildingDoor));
         ChoseRandomOwnerHouse();
         SetHouseOnMustacheometer();
         owner.transform.position = new Vector2(-15, 20);
         owner.SetActive(false);
-    }
-
-    private void SetNecessaryObjects()
-    {
-        mustacheometer = (Mustachometer)FindObjectOfType(typeof(Mustachometer));
-        owner = GameObject.FindGameObjectWithTag(Constants.owner_tag);
-        Debug.Log(owner == null);
     }
 
     private void ChoseRandomOwnerHouse()
@@ -54,9 +30,8 @@ public class GenerateOwner : MonoBehaviour
 
     private void SetHouseOnMustacheometer()
     {
-        mustacheometer.SetOwnerPosition(chosenHouse.transform);
+        mustacheometer.GetComponent<Mustachometer>().SetOwnerPosition(chosenHouse.transform);
         owner.GetComponent<OwnerSettings>().SetHouse(chosenHouse.gameObject);
         chosenHouse.GetComponent<BuildingSettings>().SetIsOwnerHouse(true);
     }
 }
-
